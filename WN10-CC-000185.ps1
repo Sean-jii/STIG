6a328cd4bbd5a4ac977fd4e90588f9cdacd6eb4f
Sean Mohr <seanryanbkmohr@gmail.com>
2025-12-29 14:29:28 -0600

    Create  WN10-CC-000185.ps1

---
+++ WN10-CC-000185.ps1
@@ -0,0 +1,44 @@
+ .NOTES
+    Author          : Sean Mohr
+    LinkedIn        : https://www.linkedin.com/in/sean-mohr-22a08b1b0/
+    GitHub          : https://github.com/Sean-jii
+    Date Created    : 2025-12-29
+    Last Modified   : 2025-12-29
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000185
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\WN10-CC-000185.ps1
+
+
+#># STIG ID: WN10-CC-000185
+# Disable Autorun on all drives
+
+$RegPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer"
+$ValueName = "NoViewOnDrive"
+$ValueData = 0xFF  # 255 - Disable autorun on all drives
+
+# Create registry path if it does not exist
+if (-not (Test-Path $RegPath)) {
+    New-Item -Path $RegPath -Force | Out-Null
+}
+
+# Apply the STIG setting
+New-ItemProperty `
+    -Path $RegPath `
+    -Name $ValueName `
+    -PropertyType DWord `
+    -Value $ValueData `
+    -Force | Out-Null
+
+Write-Output "STIG WN10-CC-000185 applied: Autorun disabled on all drives." 
