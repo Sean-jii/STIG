05eb7b3f515046b0fa06a22521621beda101b995
Sean Mohr <seanryanbkmohr@gmail.com>
2025-12-31 20:55:51 -0600

    Create WN10-CC-000290.ps1

---
+++ WN10-CC-000290.ps1
@@ -0,0 +1,33 @@
+  <#
+.NOTES
+    Author          : Sean Mohr
+    LinkedIn        : https://www.linkedin.com/in/sean-mohr-22a08b1b0/
+    GitHub          : https://github.com/Sean-jii
+    Date Created    : 2025-12-31
+    Last Modified   : 2025-12-31
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000290
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\WN10-CC-000290.ps1
+#>
+
+# Disable Windows Ink Workspace (STIG WN10-CC-000290)
+
+$Path = "HKLM:\SOFTWARE\Policies\Microsoft\WindowsInkWorkspace"
+
+If (!(Test-Path $Path)) {
+    New-Item -Path $Path -Force | Out-Null
+}
+
+New-ItemProperty -Path $Path -Name "AllowWindowsInkWorkspace" -PropertyType DWord -Value 0 -Force
